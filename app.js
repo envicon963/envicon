974d87120f51313bc81575ee306c38f935273b0a
Shamoel Ahmad <shamoel.ahmad@nielsen.com>
2025-08-04 12:04:57 +0530

    set path on app.js for vercel

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res){
     res.render("home");
